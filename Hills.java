c2533199ce29a94dc94a4af2b362f37664824b59
sheethalym <sheethal.xworkz@gmail.com>
2021-12-28 21:31:15 +0530

    adding initialsing name in the indexprogram

---
+++ Hills.java
@@ -0,0 +1,11 @@
+class Hills{
+	public static void main(String[] args){
+		for(int num=1;num<=6;num=num+1){
+			if(num==3){
+				System.out.println("nandi hills");
+			}
+			System.out.println(num);
+		}
+			
+	}
+}
