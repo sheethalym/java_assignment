f73da4fd3943480f462098a80d719fbc68e3f5d1
sheethalym <sheethal.xworkz@gmail.com>
2021-12-28 21:27:56 +0530

    adding factorialseries program

---
+++ Factorialseries.java
@@ -0,0 +1,11 @@
+class Factorialseries{
+	public static void main(String[] args){
+		int fact=1;
+		for(int num =1; num<=6;num=num+1){
+			fact= fact*num;
+			System.out.println(fact);
+		}
+
+	}
+
+}
