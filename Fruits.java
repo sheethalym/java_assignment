c8b7f51c067a64438264e78b9c4f0b130cb4206c
sheethalym <sheethal.xworkz@gmail.com>
2021-12-28 21:21:00 +0530

    adding string args on fruits

---
+++ Fruits.java
@@ -0,0 +1,9 @@
+class Fruits{
+  public static void main(String[] sheethal){
+    System.out.println("main() start");
+    for( int index= 0; index < sheethal.length;index++){
+      System.out.println(sheethal[index]);
+    }
+     System.out.println(" end main()");
+  }
+}
