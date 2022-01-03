4b85062a8fee2d6aa8931d7b03b5c8976355b907
sheethalym <sheethal.xworkz@gmail.com>
2022-01-03 23:30:01 +0530

    methods program to print students details using 3methods

---
+++ Studentsdetailsnulltest.java
@@ -0,0 +1,36 @@
+class Studentsdetailsnulltest{
+	public static void main(String[] args){
+		Studentsdetailsnull.printAll();
+		System.out.println();
+		Studentsdetailsnull.updatename(0,"sheethal");
+		Studentsdetailsnull.updatename(1,"rana");
+		Studentsdetailsnull.updatename(2,"naveen");
+		Studentsdetailsnull.updatename(3,"bharat");
+		Studentsdetailsnull.updatename(4,"hemanth");
+		Studentsdetailsnull.updatename(5,"anita");
+		Studentsdetailsnull.updatename(6,"deepa");
+		Studentsdetailsnull.updatename(7,"harshith");
+		Studentsdetailsnull.updatename(8,"mahesh");
+		Studentsdetailsnull.updatename(9,"radhika");
+		Studentsdetailsnull.updatename(10,"rani");
+		Studentsdetailsnull.updatename(11,"santosh");
+		Studentsdetailsnull.updatename(12,"shankar");
+		Studentsdetailsnull.updatename(13,"soumya");
+		Studentsdetailsnull.updatename(14,"ullas");
+		Studentsdetailsnull.updatename(15,"umesh");
+		Studentsdetailsnull.updatename(16,"arun");
+		Studentsdetailsnull.updatename(17,"shweta");
+		Studentsdetailsnull.updatename(18,"muskan");
+		Studentsdetailsnull.updatename(19,"prabhu");
+		Studentsdetailsnull.updatename(20,"akshay");
+		Studentsdetailsnull.updatename(21,"lakappa");
+		Studentsdetailsnull.updatename(22,"prashanth");
+		Studentsdetailsnull.updatename(23,"mohan");
+		Studentsdetailsnull.printAll();
+		System.out.println();
+		Studentsdetailsnull.update();
+		Studentsdetailsnull.printAll();
+		
+	}
+
+}
