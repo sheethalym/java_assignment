229abeba3c6bef7d735a549dd143fe172041895a
sheethalym <sheethal.xworkz@gmail.com>
2022-01-03 23:26:47 +0530

    methods program on printing students details

---
+++ Studentsdetailstest.java
@@ -0,0 +1,9 @@
+class Studentsdetailstest{
+	public static void main(String[] args){
+		Studentsdetails.printAll();
+		System.out.println();
+		Studentsdetails.updatenames("naveen","nemo");
+		Studentsdetails.printAll();
+	}
+
+}
