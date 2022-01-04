5e9caaf7f894200ab017f9e8bde384b03a83899f
sheethalym <sheethal.xworkz@gmail.com>
2022-01-05 00:59:37 +0530

    methods program on cars behaviours

---
+++ Carsbehaviours.java
@@ -0,0 +1,64 @@
+class Carsbehaviours{
+	static void Travel(){
+		System.out.println("cars behaviours on travel");
+	}
+	static void Travel(byte name){
+	System.out.println("car travels for 100km");
+	}
+	static void Travel(int journey, String detail){
+		System.out.println("journey of 100km to mysore");
+	}
+	static void Travel(String destination, int velocity){
+		System.out.println("destination reached after travelling at 60kmperhour");
+	}
+	static void speed(){
+		System.out.println("cars behaviours on speed");
+	}
+	static void speed(byte km){
+		System.out.println("the km of car travelled given");
+	}
+	static void speed(int acceleration,String path){
+		System.out.println("accelerates to path mysore");
+	}
+	static void speed(String tour,int litre ){
+		System.out.println("destination reached");
+	}
+	static void music(){
+		System.out.println("cars has a music system");
+	}
+	static void music(byte rap){
+		System.out.println("rap song is being played");
+	}
+	static void music(int length,String songs){
+		System.out.println("song length is 3min");
+	}
+	static void music(String sound,int seconds){
+		System.out.println("old songs of seconds 130 is played");
+	}
+	static void activity(){
+		System.out.println("many activities can be done inside car");
+	} 
+	static void activity(byte sleep){
+		System.out.println("we can have a nap inside car");
+	} 
+	static void activity(int tv,String movie){
+		System.out.println("we can watch movies in car");
+	}
+	static void activity(String party,int value){
+		System.out.println("we can listen songs and enjoy the ride");
+	}
+	static void gears(){
+		System.out.println("car has different gears");
+	}
+	static void gears(byte number){
+		System.out.println("they are six gears");
+	}
+	static void gears(int top, String high){
+		System.out.println("cars top gear is fivth gear");
+	}
+	static void gears(String reverse,int R){
+		System.out.println("car uses reverse gear to go backwards");
+	}
+	
+	
+}
