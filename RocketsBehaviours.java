897284b51d9a47d4a47f9846443f47b095c384de
sheethalym <sheethal.xworkz@gmail.com>
2022-01-05 01:01:05 +0530

    methods program on rockets behaviours

---
+++ RocketsBehaviours.java
@@ -0,0 +1,62 @@
+class RocketsBehaviours{
+	static void travels(){
+		System.out.println("destination to travels given");
+	}
+	static void travels(byte planet){
+		System.out.println("rockets should go to all travels");
+	}
+	static void travels(int num,String mars){
+		System.out.println("rockets goes to planet mars");
+	}
+	static void travels(String venus,int number){
+		System.out.println("rockets goes to venus");
+	}
+	static void launchsite(){
+		System.out.println("launchsites are located");
+	}
+	static void launchsite(byte launchno){
+		System.out.println("rocket launches from no1 site");
+	}
+	static void launchsite(int no,String platform){
+		System.out.println("rocket launches from platform");
+	}
+	static void launchsite(String platform1,int siteno){
+		System.out.println("rocket launches from platform1");
+	}
+	static void fuels(){
+		System.out.println("fuels used in rockets");
+	}
+	static void fuels(byte price){
+		System.out.println("price of fuels for rockets is highpriced");
+	}
+	static void fuels(int money,String bunk){
+		System.out.println("hydrazine is one of the fuels used");
+	}
+	static void fuels(String petrol,int amount){
+		System.out.println("liquid hydrogen is the signature fuel of space program");
+	}
+	static void names(){
+		System.out.println("list of names of rockets");
+	}
+	static void names(byte code){
+		System.out.println("chandrayaan rocket is controlled by headquartes located at pincode 560094");
+	}
+	static void names(int pincode,String headquarters){
+		System.out.println("chandrayaan was location");
+	}
+	static void names(String quarters,int pin){
+		System.out.println("every rockets is controlled by headquartes");
+	}
+	static void distance(){
+		System.out.println("distance travelled by rockets");
+	}
+	static void distance(byte km){
+		System.out.println("distance travelled by rocket is 1975646km");
+	}
+	static void distance(int meter,String path){
+		System.out.println("distance travelled by rockets to path moon is 197555km");
+	}
+	static void distance(String route,int speed){
+		System.out.println("distance travelled by rocket tp moon is at speed of 10000kmpersec");
+	}
+}
