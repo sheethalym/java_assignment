e9a8fd284651b0bffe6c984fbe0a4e666b177471
sheethalym <sheethal.xworkz@gmail.com>
2022-01-12 00:38:41 +0530

    program on object array

---
+++ Details.java
@@ -0,0 +1,14 @@
+class Details{
+	int id;
+	String name;
+	String email;
+	long number;
+	int age;
+	Details(int studentid,String fullname,String emailof,long phnumber,int ageof){
+		id=studentid;
+		name=fullname;
+		email=emailof;
+		number=phnumber;
+		age=ageof; 
+	}
+}
