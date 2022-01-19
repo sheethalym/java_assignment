a51f0ea7b2d06dd2f66cd0663952524e9c929ae9
sheethalym <sheethal.xworkz@gmail.com>
2022-01-20 00:44:11 +0530

    program of typecasting on char datatype

---
+++ CharConversion.java
@@ -0,0 +1,29 @@
+class CharConversion{
+	public static void main(String[] args){
+		//narrowing conversion
+		char symbol='b';
+		byte sym=(byte)symbol;
+		System.out.println(sym);
+		//narrowing conversion
+		char symbol1='s';
+		short sym1=(short)symbol1;
+		System.out.println(sym1);
+		//narrowing conversion
+		char symbol2='r';
+		int sym2=symbol2;
+		System.out.println(symbol2);
+		//narrowing conversion
+		char symbol3='n';
+		long sym3=symbol3;
+		System.out.println(symbol3);
+		//narrowing conversion
+		char symbol4='h';
+		float sym4=symbol4;
+		System.out.println(symbol4);
+		//narrowing conversion
+		char symbol5='p';
+		double sym5=symbol5;
+		System.out.println(symbol5);
+		
+	}
+}
