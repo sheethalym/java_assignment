70b472c8e8bcc11ee9d38ee7a7456626d9539c9a
sheethalym <sheethal.xworkz@gmail.com>
2022-01-23 22:30:40 +0530

    wapper class from string to double

---
+++ StringDouble.java
@@ -0,0 +1,8 @@
+class StringDouble{
+	public static void main(String[] args){
+		for(int index=0;index<args.length;index++){
+			double kd=Double.parseDouble(args[index]);
+			System.out.println(kd);
+		}
+	}
+}
