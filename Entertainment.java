4512bf0e0207eb138fc2a14a29fa5c98768f5a22
sheethalym <sheethal.xworkz@gmail.com>
2022-01-23 22:24:28 +0530

    adding class program

---
+++ Entertainment.java
@@ -0,0 +1,12 @@
+class Entertainment{
+	
+	String type;
+	float duration;
+	boolean interest;
+	
+	Entertainment(String typee,float durationn,boolean interests){
+		type=typee;
+		duration=durationn;
+		interest=interests;
+	}
+}
