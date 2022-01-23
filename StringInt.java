3857657dd52f45ede57e096c318c5ef6e0dbeb08
sheethalym <sheethal.xworkz@gmail.com>
2022-01-23 22:29:00 +0530

    wapper class from string to int

---
+++ StringInt.java
@@ -0,0 +1,8 @@
+class StringInt{
+	public static void main(String[] args){
+		for(int index=0;index<args.length;index++){
+			int bal=Integer.parseInt(args[index]);
+			System.out.println(bal);
+		}
+	}
+}
